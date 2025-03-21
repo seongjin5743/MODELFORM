9d781f6a5a11482b2def85c743af6a2583f19c42
seongjin <seongjin5743@naver.com>
2025-03-21 09:57:58 +0900

    모델링, 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 처음 저장할 때만 시간 저장
+    updated_at = models.DateTimeField(auto_now=True) # 업데이트 할 때마다 시간 저장
