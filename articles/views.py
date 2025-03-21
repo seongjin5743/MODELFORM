03b846eceb492f48cf59fce3e467589476bf5f72
seongjin <seongjin5743@naver.com>
2025-03-21 12:04:06 +0900

    Create(ver.2)

--- articles/views.py
+++ articles/views.py
@@ -16,22 +16,37 @@ def create(request):
     # =========================
     # GET create/ 빈 종이를 보여주기
     # POST create/ 사용자가 입력한 데이터를 저장
+
+    # 모든 경우의 수
+    # GET: form을 만들어서 html 문서를 사용자에게 리턴
+    # POST: invaild 검증 실패
+    # POST: valid 겅증 성공
     
+    # POST 요청
     if request.method == 'POST':
+        # POST 사용자가 입력한 데이터를 담은 폼을 만든다.
         form = ArticleForm(request.POST)
+        # POST 검증 성공.
         if form.is_valid():
+            # POST 폼을 저장.
             form.save()
+            # POST index로 리다이렉트.
             return redirect('articles:index')
-        else:
-            context = {
-                'form': form,
-            }
-            return render(request, 'create.html', context)
-
+        # POST 검증 실패.
+        # else:
+        #     context = {
+        #         'form': form,
+        #     }
+        #     return render(request, 'create.html', context)
+        # 밑에 있는 else 문과 같기 때문에 지운다.
+    
+    # GET 요청
     else:
+        # GET 비어있는 폼을 만든다.
         form = ArticleForm()
-
-        context = {
-            'form': form,
-        }
-        return render(request, 'create.html', context)
+    # GET context에 비어있는 폼을 만든다.
+    # POST 검증 실패한다면 context에 입력한 데이터를 담은 폼을 만든다.
+    context = {
+        'form': form,
+    }
+    return render(request, 'create.html', context)
